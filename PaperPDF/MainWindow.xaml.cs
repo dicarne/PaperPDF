5d219e50962722a0122ee2fd1ddcbd4d4f39b917
dicarne <hzdingjiahe@outlook.com>
2020-07-24 15:52:36 +0800

    init commit

---
+++ PaperPDF/MainWindow.xaml.cs
@@ -0,0 +1,383 @@
+﻿using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.IO;
+using Image = System.Windows.Controls.Image;
+using System.Windows.Input;
+using System.Collections.Generic;
+using YamlDotNet.Serialization;
+using System;
+using System.Windows.Ink;
+using System.Windows.Media;
+
+namespace PaperPDF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string file_path = "";
+        string note_path => file_path + ".notes";
+        InkNoteSaveData inkNoteSaveData = new InkNoteSaveData();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            scrollViewer.AddHandler(MouseLeftButtonDownEvent, new MouseButtonEventHandler(scrollViewer_MouseLeftButtonDown), true);
+            scrollViewer.AddHandler(MouseLeftButtonUpEvent, new MouseButtonEventHandler(scrollViewer_MouseLeftButtonUp), true);
+
+            MainInkCanvas.AddHandler(MouseLeftButtonDownEvent, new MouseButtonEventHandler(MainInkCanvas_MouseLeftButtonDown), true);
+            MainInkCanvas.AddHandler(MouseLeftButtonUpEvent, new MouseButtonEventHandler(MainInkCanvas_MouseLeftButtonUp), true);
+
+
+            MainInkCanvas.DefaultDrawingAttributes.FitToCurve = true;
+
+            string[] command = Environment.GetCommandLineArgs();//获取进程命令行参数
+            if (command.Length > 1)
+            {
+                file_path = command[1];
+            }
+            if (!file_path.EndsWith(".pdf"))
+            {
+                MessageBox.Show("无法打开非PDF文件");
+                App.Current.Shutdown();
+            }
+
+            pdf = new MuPdf(file_path);
+            // read note data
+            if (File.Exists(note_path))
+            {
+                try
+                {
+                    inkNoteSaveData = new Deserializer().Deserialize<InkNoteSaveData>(File.ReadAllText(note_path));
+                }
+                catch (System.Exception)
+                {
+                    MessageBox.Show("笔记打开失败！");
+                }
+            }
+            else
+            {
+                inkNoteSaveData = new InkNoteSaveData();
+            }
+            if (inkNoteSaveData.allnotes.Count == 0) { inkNoteSaveData.allnotes.Add(new InkNoteData()); }
+
+            // 
+
+            var index = 0;
+            var top = 0.0;
+            foreach (var item in pdf.pages)
+            {
+                var scale = MainInkCanvas.Width / item.width;
+
+                var image = new Image
+                {
+                    Width = item.width * scale,
+                    Height = item.height * scale,
+                };
+                item.scale = scale;
+                item.Top = top;
+                MainInkCanvas.Children.Add(image);
+                InkCanvas.SetTop(image, top);
+                top += item.height * scale;
+                index++;
+            }
+
+        }
+
+
+        MuPdf pdf;
+
+        public async Task LoadPage(Image image, int i)
+        {
+            if (!pdf.pages[i].loading)
+            {
+                pdf.pages[i].loading = true;
+                var bitmap = pdf.RenderAPage(i);
+                var bimage = await Task.Run(() =>
+               {
+                   if (!pdf.pages[i].unload)
+                   {
+
+                       MemoryStream ms = new MemoryStream();
+                       bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+                       byte[] bytes = ms.GetBuffer();  //byte[]   bytes=   ms.ToArray(); 这两句都可以
+                       ms.Close();
+
+                       //Convert it to BitmapImage
+                       var newms = new MemoryStream(bytes);
+                       BitmapImage bimage = new BitmapImage();
+                       bimage.BeginInit();
+                       bimage.StreamSource = newms;
+                       bimage.EndInit();
+
+                       bimage.Freeze();
+                       bitmap.Dispose();
+                       return bimage;
+                   }
+                   return null;
+               });
+
+                if (!pdf.pages[i].unload)
+                {
+                    image.Source = bimage;
+                }
+
+                pdf.pages[i].loading = false;
+            }
+
+        }
+
+        bool loaded = false;
+        private void MainInkCanvas_Loaded(object sender, RoutedEventArgs e)
+        {
+            CheckInView(0, scrollViewer.ViewportHeight);
+            loaded = true;
+            if (inkNoteSaveData.allnotes[inkNoteSaveData.Current].data != null)
+            {
+                MainInkCanvas.Strokes = new StrokeCollection(new MemoryStream(inkNoteSaveData.allnotes[inkNoteSaveData.Current].data));
+            }
+            CheckPreButton();
+            scrollViewer.ScrollToVerticalOffset(inkNoteSaveData.TopOffset);
+
+        }
+
+        void CheckPreButton()
+        {
+            if (inkNoteSaveData.Current == 0)
+            {
+                PreButton.IsEnabled = false;
+            }
+            else
+            {
+                PreButton.IsEnabled = true;
+            }
+        }
+
+        public void CheckInView(double topoffset, double viewHeight)
+        {
+
+            var range = FindPage(topoffset, viewHeight);
+            if (range[0] - 1 >= 0) range[0]--;
+            if (range[1] + 1 < pdf.pages.Count) range[1]++;
+            for (int i = 0; i < range[0]; i++)
+            {
+                var image = GetImageWithPage(i);
+                image.Source = null;
+                pdf.pages[i].unload = true;
+            }
+            for (int i = range[0]; i <= range[1]; i++)
+            {
+                var image = GetImageWithPage(i);
+                pdf.pages[i].unload = false;
+                if (image.Source == null)
+                {
+                    LoadPage(image, i);
+                }
+            }
+            for (int i = range[1] + 1; i < pdf.pages.Count; i++)
+            {
+                var image = GetImageWithPage(i);
+                image.Source = null;
+                pdf.pages[i].unload = true;
+            }
+
+        }
+
+        public Image GetImageWithPage(int page)
+        {
+            Image img;
+            page--;
+            do
+            {
+                page++;
+                img = MainInkCanvas.Children[page] as Image;
+            } while (img == null);
+            return img;
+        }
+
+        public int[] FindPage(double offset, double viewHeight)
+        {
+            var res = new int[2] { int.MaxValue, 0 };
+            var Pages = pdf.pages;
+
+            for (int i = 0; i < Pages.Count; i++)
+            {
+                var scale = Pages[i].scale;
+                if (Pages[i].Top <= offset && Pages[i].Top + Pages[i].height * scale >= offset)
+                {
+                    res[0] = i;
+                }
+                if (Pages[i].Top + Pages[i].height * scale >= offset + viewHeight)
+                {
+                    res[1] = i;
+                    break;
+                }
+            }
+            return res;
+        }
+
+        private void scrollViewer_ScrollChanged(object sender, ScrollChangedEventArgs e)
+        {
+            if (loaded)
+            {
+                var top = e.VerticalOffset;
+                CheckInView(top, e.ViewportHeight);
+            }
+
+        }
+
+        Point mousePoint;
+        private void scrollViewer_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (!inCanvas)
+            {
+                dragging = true;
+                mousePoint = e.GetPosition(scrollViewer);
+            }
+        }
+
+        bool inCanvas = false;
+        bool dragging = false;
+        private void MainInkCanvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            inCanvas = true;
+        }
+
+        private void MainInkCanvas_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            inCanvas = false;
+        }
+
+
+        private void scrollViewer_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            dragging = false;
+            inCanvas = false;
+        }
+
+        private void PdfGrid_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (dragging)
+            {
+                dragging = false;
+                inCanvas = false;
+            }
+        }
+
+        private void scrollViewer_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (dragging && !inCanvas)
+            {
+                var curPosition = e.GetPosition(scrollViewer);
+                var delta = curPosition - mousePoint;
+                mousePoint = curPosition;
+                scrollViewer.ScrollToVerticalOffset(scrollViewer.VerticalOffset - delta.Y * 1.5);
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            inkNoteSaveData.TopOffset = scrollViewer.VerticalOffset;
+            SaveStrokes();
+            SaveToFile();
+        }
+
+        void SaveStrokes()
+        {
+            var stokes = MainInkCanvas.Strokes;
+            var stm = new MemoryStream();
+            stokes.Save(stm);
+            inkNoteSaveData.allnotes[inkNoteSaveData.Current].data = stm.ToArray();
+            stm.Close();
+        }
+
+        void SaveToFile()
+        {
+            var ser = new Serializer();
+            var str = ser.Serialize(inkNoteSaveData);
+            File.WriteAllText(note_path, str);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // pre
+            SaveStrokes();
+            inkNoteSaveData.Current -= 1;
+            if (inkNoteSaveData.Current < 0) inkNoteSaveData.Current = 0;
+            MainInkCanvas.Strokes.Clear();
+            if (inkNoteSaveData.allnotes[inkNoteSaveData.Current].data != null)
+                MainInkCanvas.Strokes = new StrokeCollection(new MemoryStream(inkNoteSaveData.allnotes[inkNoteSaveData.Current].data));
+
+            CheckPreButton();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            // next
+            SaveStrokes();
+            inkNoteSaveData.Current += 1;
+            if (inkNoteSaveData.Current >= inkNoteSaveData.allnotes.Count)
+            {
+                inkNoteSaveData.allnotes.Add(new InkNoteData());
+            }
+            MainInkCanvas.Strokes.Clear();
+            if (inkNoteSaveData.allnotes[inkNoteSaveData.Current].data != null)
+                MainInkCanvas.Strokes = new StrokeCollection(new MemoryStream(inkNoteSaveData.allnotes[inkNoteSaveData.Current].data));
+
+            CheckPreButton();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            // black
+            MainInkCanvas.EditingMode = InkCanvasEditingMode.Ink;
+            var da = new DrawingAttributes();
+            da.Color = Colors.Black;
+            MainInkCanvas.DefaultDrawingAttributes = da;
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            // red
+            MainInkCanvas.EditingMode = InkCanvasEditingMode.Ink;
+            var da = new DrawingAttributes();
+            da.Color = Colors.Red;
+            MainInkCanvas.DefaultDrawingAttributes = da;
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            // earse
+            MainInkCanvas.EditingMode = InkCanvasEditingMode.EraseByStroke;
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            // 荧光笔
+            MainInkCanvas.EditingMode = InkCanvasEditingMode.Ink;
+            var da = new DrawingAttributes();
+            da.Color = Colors.Yellow;
+            da.IsHighlighter = true;
+            da.IgnorePressure = true;
+            da.StylusTip = StylusTip.Rectangle;
+            da.Height = 30;
+            da.Width = 10;
+            MainInkCanvas.DefaultDrawingAttributes = da;
+        }
+    }
+
+    class InkNoteSaveData
+    {
+        public int Current;
+        public double TopOffset;
+        public List<InkNoteData> allnotes = new List<InkNoteData>();
+    }
+    class InkNoteData
+    {
+        public byte[] data;
+    }
+}
---
+++ PaperPDF/MuPdf.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace PaperPDF
+{
+    class PageRef
+    {
+        public IntPtr pagePtr;
+        public int ID;
+        public float width, height;
+        public double scale;
+        public double Top;
+        public bool loading;
+        public bool unload;
+    }
+    class MuPdf
+    {
+        IntPtr ctx;
+        IntPtr doc;
+        IntPtr stm;
+        public List<PageRef> pages;
+        public MuPdf(string path)
+        {
+            pages = new List<PageRef>();
+            const uint FZ_STORE_DEFAULT = 256 << 20;
+            ctx = NativeMethods.NewContext(IntPtr.Zero, IntPtr.Zero, FZ_STORE_DEFAULT, "1.17.0"); // 创建上下文
+            NativeMethods.fz_register_document_handlers(ctx);
+            stm = NativeMethods.OpenFile(ctx, path); // 打开文件流
+            doc = NativeMethods.OpenDocumentStream(ctx, ".pdf", stm); // 从文件流创建文档对象
+            int pn = NativeMethods.CountPages(ctx, doc); // 获取文档的页数
+            for (int i = 0; i < pn; i++)
+            { 
+                // 遍历各页
+                IntPtr p = NativeMethods.LoadPage(ctx, doc, i); // 加载页面（首页为 0）
+                Rectangle b = NativeMethods.BoundPage(ctx, p); // 获取页面尺寸
+                int width = (int)(b.Right - b.Left); // 获取页面的宽度和高度
+                int height = (int)(b.Bottom - b.Top);
+                pages.Add(new PageRef()
+                {
+                    ID = i,
+                    pagePtr = p,
+                    width = width,
+                    height = height
+                });
+            }
+        }
+
+        public Bitmap RenderAPage(int pageID)
+        {
+            return RenderPage(ctx, doc, pages[pageID].pagePtr, pages[pageID].scale, 2);
+        }
+
+        ~MuPdf()
+        {
+            for (int i = 0; i < pages.Count; i++)
+            { // 遍历各页
+                IntPtr p = pages[i].pagePtr;
+                NativeMethods.FreePage(ctx, p); // 释放页面所占用的资源
+            }
+            NativeMethods.CloseDocument(ctx, doc); // 释放其它资源
+            NativeMethods.CloseStream(ctx, stm);
+            NativeMethods.FreeContext(ctx);
+        }
+
+        static Bitmap RenderPage(IntPtr context, IntPtr document, IntPtr page, double scale, double zoom)
+        {
+            Matrix ctm = new Matrix();
+            IntPtr pix = IntPtr.Zero;
+            IntPtr dev = IntPtr.Zero;
+
+
+            //ctm.A = ctm.D = 1; // 设置单位矩阵 (1,0,0,1,0,0)
+
+            zoom = zoom * scale;
+
+            ctm = NativeMethods.fz_scale((float)zoom, (float)zoom);
+            //ctm = NativeMethods.fz_pre_rotate(ctm, 0);
+            var c = NativeMethods.FindDeviceColorSpace(context);
+            pix = NativeMethods.fz_new_pixmap_from_page(
+                context, page, ctm, c, 0);
+            int width = NativeMethods.fz_pixmap_width(context, pix);
+            int height = NativeMethods.fz_pixmap_height(context, pix);
+
+            // 创建与 Pixmap 相同尺寸的彩色 Bitmap
+            Bitmap bmp = new Bitmap(width, height, PixelFormat.Format24bppRgb);
+            var imageData = bmp.LockBits(new System.Drawing.Rectangle(0, 0,
+                              width, height), ImageLockMode.ReadWrite, bmp.PixelFormat);
+
+            unsafe
+            { // 将 Pixmap 的数据转换为 Bitmap 数据
+              // 获取  的图像数据
+                byte* ptrSrc = (byte*)NativeMethods.GetSamples(context, pix);
+                byte* ptrDest = (byte*)imageData.Scan0;
+                for (int y = 0; y < height; y++)
+                {
+                    byte* pl = ptrDest;
+                    byte* sl = ptrSrc;
+                    for (int x = 0; x < width; x++)
+                    {
+                        // 将 Pixmap 的色彩数据转换为 Bitmap 的格式
+                        pl[2] = sl[0]; //b-r
+                        pl[1] = sl[1]; //g-g
+                        pl[0] = sl[2]; //r-b
+                                       //sl[3] 是透明通道数据，在此忽略
+                        pl += 3;
+                        sl += 3;
+                    }
+                    ptrDest += imageData.Stride;
+                    ptrSrc += width * 3;
+                }
+            }
+            NativeMethods.DropPixmap(context, pix); // 释放 Pixmap 占用的资源
+            return bmp;
+        }
+    }
+
+
+    public struct BBox
+    {
+        public int Left, Top, Right, Bottom;
+    }
+    public struct Rectangle
+    {
+        public float Left, Top, Right, Bottom;
+    }
+    public struct Matrix
+    {
+        public float A, B, C, D, E, F;
+    }
+    class NativeMethods
+    {
+
+        const string DLL = "libmupdf.dll";
+
+        [DllImport(DLL, EntryPoint = "fz_new_context_imp", CharSet = CharSet.Ansi)]
+        public static extern IntPtr NewContext(IntPtr alloc, IntPtr locks, uint max_store, string version = "1.17.0");
+
+        [DllImport(DLL, EntryPoint = "fz_free_context")]
+        public static extern IntPtr FreeContext(IntPtr ctx);
+
+        [DllImport(DLL, EntryPoint = "fz_open_file_w", CharSet = CharSet.Unicode)]
+        public static extern IntPtr OpenFile(IntPtr ctx, string fileName);
+
+        [DllImport(DLL, EntryPoint = "fz_open_document_with_stream", CharSet = CharSet.Ansi)]
+        public static extern IntPtr OpenDocumentStream(IntPtr ctx, string magic, IntPtr stm);
+
+        [DllImport(DLL, EntryPoint = "fz_close")]
+        public static extern IntPtr CloseStream(IntPtr ctx, IntPtr stm);
+
+        [DllImport(DLL, EntryPoint = "fz_close_document")]
+        public static extern IntPtr CloseDocument(IntPtr ctx, IntPtr doc);
+
+        [DllImport(DLL, EntryPoint = "fz_count_pages")]
+        public static extern int CountPages(IntPtr ctx, IntPtr doc);
+
+        [DllImport(DLL, EntryPoint = "fz_bound_page")]
+        public static extern Rectangle BoundPage(IntPtr ctx, IntPtr page);
+
+        [DllImport(DLL, EntryPoint = "fz_clear_pixmap_with_value")]
+        public static extern void ClearPixmap(IntPtr ctx, IntPtr pix, int byteValue);
+
+        [DllImport(DLL, EntryPoint = "fz_page_separations")]
+        public static extern IntPtr fz_page_separations(IntPtr ctx, IntPtr page);
+
+        [DllImport(DLL, EntryPoint = "fz_device_rgb")]
+        public static extern IntPtr FindDeviceColorSpace(IntPtr ctx);
+
+        [DllImport(DLL, EntryPoint = "fz_free_device")]
+        public static extern void FreeDevice(IntPtr ctx, IntPtr dev);
+
+        [DllImport(DLL, EntryPoint = "fz_close_device")]
+        public static extern void CloseDevice(IntPtr ctx, IntPtr dev);
+
+        [DllImport(DLL, EntryPoint = "fz_free_page")]
+        public static extern void FreePage(IntPtr doc, IntPtr page);
+
+        [DllImport(DLL, EntryPoint = "fz_load_page")]
+        public static extern IntPtr LoadPage(IntPtr ctx, IntPtr doc, int pageNumber);
+
+        [DllImport(DLL, EntryPoint = "fz_new_draw_device")]
+        public static extern IntPtr NewDrawDevice(IntPtr ctx, Matrix ind, IntPtr pix);
+
+        [DllImport(DLL, EntryPoint = "fz_new_pixmap")]
+        public static extern IntPtr NewPixmap(IntPtr ctx, IntPtr colorspace, int width, int height, IntPtr seps, int alpha);
+
+        [DllImport(DLL, EntryPoint = "fz_run_page")]
+        public static extern void RunPage(IntPtr ctx, IntPtr doc, IntPtr page, IntPtr dev, Matrix transform, IntPtr cookie);
+
+        [DllImport(DLL, EntryPoint = "fz_drop_pixmap")]
+        public static extern void DropPixmap(IntPtr ctx, IntPtr pix);
+
+        [DllImport(DLL, EntryPoint = "fz_pixmap_samples")]
+        public static extern IntPtr GetSamples(IntPtr ctx, IntPtr pix);
+
+        [DllImport(DLL, EntryPoint = "fz_register_document_handlers")]
+        public static extern IntPtr fz_register_document_handlers(IntPtr ctx);
+
+        [DllImport(DLL, EntryPoint = "fz_new_pixmap_from_page")]
+        public static extern IntPtr fz_new_pixmap_from_page(IntPtr ctx, IntPtr pag, Matrix ctm, IntPtr colorspace, int aplha);
+
+        [DllImport(DLL, EntryPoint = "fz_scale")]
+        public static extern Matrix fz_scale(float a, float b);
+
+        [DllImport(DLL, EntryPoint = "fz_pre_rotate")]
+        public static extern Matrix fz_pre_rotate(Matrix a, float degree);
+
+        [DllImport(DLL, EntryPoint = "fz_pixmap_width")]
+        public static extern int fz_pixmap_width(IntPtr doc, IntPtr pix);
+        [DllImport(DLL, EntryPoint = "fz_pixmap_height")]
+        public static extern int fz_pixmap_height(IntPtr doc, IntPtr pix);
+    }
+}
